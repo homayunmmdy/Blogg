d0e31d535befd39b4af4333dd52e4fb229f56f5c
Homayoun <homayoun763@gmail.com>
2024-03-28 18:10:34 +0330

    footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Link from "next/link";
+import SiteConfig from "../config/site";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-center p-10 bg-base-100 text-base-content rounded">
+      <ul className="grid grid-flow-col gap-4">
+        {SiteConfig.nav.map((nav) => (
+          <li key={nav.id}>
+            <Link href={nav.href} className="link link-hover">
+              {nav.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <nav>
+        <div className="grid grid-flow-col gap-4">
+          <Link href="/">
+            <FaFacebookF size={22} />
+          </Link>
+          <Link href="/">
+            <FaInstagram size={22} />
+          </Link>
+          <Link href="/">
+            <FaXTwitter size={22} />
+          </Link>
+        </div>
+      </nav>
+      <aside>
+        <p>کپی رایت © {new Date().getFullYear()} -تمامی حقوق برای {SiteConfig.name} محفوظ است</p>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import  SiteConfig from "@/app/config/site"
+import SiteConfig from "@/app/config/site";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,10 +12,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang={SiteConfig.lang} dir={SiteConfig.dir}  >
+    <html lang={SiteConfig.lang} dir={SiteConfig.dir}>
       <body className={inter.className}>
         <Navbar />
-        {children}</body>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
